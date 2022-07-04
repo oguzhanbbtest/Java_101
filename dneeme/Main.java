3dcb4dad1bdddbe5efa2fd6b68d5c71f87681733
Oğuzhan Bahadır Bayram <105600982+oguzhanbbtest@users.noreply.github.com>
2022-07-05 00:16:29 +0300

    Delete dneeme directory

--- dneeme/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public public static void main(String[] args) {
-
-        int ogrenciSayisi = 10;
-        System.out.println("Öğrenci Sayım :" + ogrenciSayisi);
-
-    }
-
-}
